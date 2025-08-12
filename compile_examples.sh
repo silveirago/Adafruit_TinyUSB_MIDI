593c026a9d3985094e02a9441cc9938707341a15
Gustavo Silveira <silveira.go@gmail.com>
2025-08-11 22:39:25 -0300

    Add SysEx host test to build script

--- compile_examples.sh
+++ compile_examples.sh
@@ -17,8 +17,12 @@ mkdir -p build
 
 echo "Compiling TinyUSB host tests"
 g++ -std=c++17 -I. -Itests/stubs tests/test_tinyusb_send_receive.cpp Adafruit_TinyUSB_MIDI.cpp -o build/test_tinyusb
-./build/test_tinyusb
+./build/test_tinyusb || exit 1
+
+echo "Compiling SysEx host tests"
+g++ -std=c++17 -I. -Itests/stubs tests/test_sysex_receive.cpp Adafruit_TinyUSB_MIDI.cpp -o build/test_sysex
+./build/test_sysex || exit 1
 
 echo "Compiling Renesas host tests"
 g++ -std=c++17 -I. -Itests/stubs -DADAFRUIT_TINYUSB_MIDI_RENESAS tests/test_renesas_send.cpp Adafruit_TinyUSB_MIDI.cpp -o build/test_renesas
-./build/test_renesas
+./build/test_renesas || exit 1
